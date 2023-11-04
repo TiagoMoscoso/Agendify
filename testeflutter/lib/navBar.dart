5c04e6ba7f06f4648af61f89533aaee52fdf4763
Pedro Caillaux <76521214+phc2603@users.noreply.github.com>
2023-11-04 14:22:27 -0300

    Update navBar.dart

--- testeflutter/lib/navBar.dart
+++ testeflutter/lib/navBar.dart
@@ -3,25 +3,31 @@ import 'perfil.dart';
 import 'agenda.dart';
 import 'searchBar.dart';
 import 'mainscreen.dart';
+import 'package:testeflutter/Classes/ClassUser.dart';
 
 class BottomNavigationBarExample extends StatefulWidget {
-  const BottomNavigationBarExample({super.key});
+  final ClassUser user;
+  const BottomNavigationBarExample({super.key, required this.user});
 
   @override
   State<BottomNavigationBarExample> createState() =>
-      _BottomNavigationBarExampleState();
+      _BottomNavigationBarExampleState(user: user);
 }
 
 class _BottomNavigationBarExampleState
     extends State<BottomNavigationBarExample> {
+  final ClassUser user; // Declare uma variável para armazenar o User
+
+  _BottomNavigationBarExampleState({required this.user}); // Construtor que recebe o User
+
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 12, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    MainScreen(),
-    SearchPage(),
-    Agenda(),
-    ProfileOriginal(),
+  static List<Widget> _widgetOptions(ClassUser user) => <Widget>[
+    const MainScreen(),
+    const SearchPage(),
+    const Agenda(),
+    Profile(user: user),
   ];
 
   void _onItemTapped(int index) {
@@ -34,7 +40,7 @@ class _BottomNavigationBarExampleState
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child: _widgetOptions(user).elementAt(_selectedIndex), // Passe o User como argumento
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
@@ -42,7 +48,7 @@ class _BottomNavigationBarExampleState
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
-            label: 'Início',
+            label: 'Inicio',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search_outlined),
@@ -56,7 +62,6 @@ class _BottomNavigationBarExampleState
             icon: Icon(Icons.person),
             label: 'Perfil',
           ),
-
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Color.fromARGB(255, 100, 123, 255),
